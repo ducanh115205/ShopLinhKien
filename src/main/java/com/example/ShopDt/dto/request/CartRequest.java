18e4ec24ad9fe0d5801b38e6184c2cb0752902a4
Nguyen Duc Anh <ngducanh11052005@gmail.com>
2026-05-15 23:38:34 +0700

    Remove user id from cart request

--- src/main/java/com/example/ShopDt/dto/request/CartRequest.java
+++ src/main/java/com/example/ShopDt/dto/request/CartRequest.java
@@ -10,9 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartRequest {
-    @NotNull(message = "UserId không được để trống")
-    private Long userId;
-
     @NotNull(message = "ProductId không được để trống")
     private Long productId;
 
