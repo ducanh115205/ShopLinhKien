b68784c6ad5990a80589088342c922499149ea76
Nguyen Duc Anh <ngducanh11052005@gmail.com>
2026-05-15 23:42:30 +0700

    Remove user id from shipment request

--- src/main/java/com/example/ShopDt/dto/request/ShipmentDetailRequest.java
+++ src/main/java/com/example/ShopDt/dto/request/ShipmentDetailRequest.java
@@ -1,15 +1,11 @@
 package com.example.ShopDt.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ShipmentDetailRequest {
     private Long id;
-
-    @NotNull(message = "UserId không được để trống")
-    private Long userId;
 
     @NotBlank(message = "Địa chỉ không được để trống")
     private String address;
@@ -19,5 +15,6 @@ public class ShipmentDetailRequest {
 
     @NotBlank(message = "Tên người nhận không được để trống")
     private String receiver;
+
     private int status = 1;
 }
