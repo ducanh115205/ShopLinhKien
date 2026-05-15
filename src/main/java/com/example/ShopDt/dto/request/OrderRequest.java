24ef6030cafb84fbf7c3c84984fb5fcf7b1925c0
Nguyen Duc Anh <ngducanh11052005@gmail.com>
2026-05-15 23:39:17 +0700

    Remove user id from order request

--- src/main/java/com/example/ShopDt/dto/request/OrderRequest.java
+++ src/main/java/com/example/ShopDt/dto/request/OrderRequest.java
@@ -1,12 +1,9 @@
 package com.example.ShopDt.dto.request;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class OrderRequest {
-    @NotNull(message = "UserId không được để trống")
-    private Long userId;
     private String note;
     private Long shipmentDetailId;
 }
